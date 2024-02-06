bad2cab4a3251f4cef219803dc3a1aa7944d40bc
Frek714 <federico.dursi1998@gmail.com>
2024-02-06 19:25:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-amber-500">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
